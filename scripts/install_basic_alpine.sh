177c40b17cdf81364dec8bb6008884d032920b74
Alexander Piskun <bigcat88@icloud.com>
2022-12-21 20:17:18 +0300

    ccache + optimizations

--- scripts/install_basic_alpine.sh
+++ scripts/install_basic_alpine.sh
@@ -10,11 +10,17 @@ apk add --no-cache \
   autoconf \
   automake \
   cmake \
-  nasm \
   python3-dev \
   py3-pip && \
   echo "**** Installing Patchelf ****" && \
   git clone -b 0.17.0 https://github.com/NixOS/patchelf.git && \
   cd patchelf && \
   ./bootstrap.sh && ./configure && make && make check && make install && \
-  cd ..
+  cd .. && rm -rf ./patchelf && \
+  echo "**** Installing ccache ****" && \
+  git clone -b v4.7.4 https://github.com/ccache/ccache.git && \
+  cd ccache && \
+  mkdir build && cd build && \
+  cmake -DCMAKE_BUILD_TYPE=Release .. && \
+  make && make install && \
+  cd ../.. && rm -rf ./ccache
